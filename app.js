a0a93951d4353bf008ddda8e78a2c150c2a74680
utkarsh-e4 <utkarshsns@gmail.com>
2021-12-22 10:35:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review:String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit"
+});
+
+// fruit.save();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "Utkarsh",
+  age: 21
+});
+
+person.save();
